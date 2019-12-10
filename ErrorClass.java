c8db233928cbf2a06bbc2e5be38be00b93229842
rmelis <57659595+rmelis@users.noreply.github.com>
2019-12-10 10:32:57 +0100

    Add files via upload

--- ErrorClass.java
+++ ErrorClass.java
@@ -2,6 +2,10 @@ package TestDecember;
 
 public class ErrorClass {
 	public static void main(String[] args) {
+		ErrorClass zin = new ErrorClass();
+		zin.PrintHelloWorld();
+	}
+	public void PrintHelloWorld(){
 		System.out.println("Hello world!");
 	}
 }
